659072762422f287c414fb7153ce54572fe5627c
Deepak <dd43028@gmail.com>
2020-06-02 23:39:39 +0530

    Updates

---
+++ precache-manifest.f26baf3e50f8b7610208773d3dc04139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59d8529750365b511864b287d408761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b5d86aebfe498e321f4",
+    "url": "/static/css/main.b39b71f2.chunk.css"
+  },
+  {
+    "revision": "6514ffceacf68e542a8c",
+    "url": "/static/js/2.42b85209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42b85209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5d86aebfe498e321f4",
+    "url": "/static/js/main.baeba8a5.chunk.js"
+  },
+  {
+    "revision": "4307f6d9ff30b2a31726",
+    "url": "/static/js/runtime-main.272e3ed3.js"
+  }
+]);
